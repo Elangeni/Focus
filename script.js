4cec9a558308167efa8e9bab287ada325a436f81
Elangeni Yabba <41975353+Elangeni@users.noreply.github.com>
2020-10-14 20:02:26 -0400

    Add files via upload
    The timer works now, as well as the functionality to increase the session and break minutes

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 var minutes = 25;
 var seconds = "00";
 
+var sessionMinutes = 25;
+var breakMinutes = 5;
+
+var flag = false;
+var temp = true;
+
 // Initialize sounds
 var click = new Audio("click.mp3");
 var bell = new Audio("bell.mp3");
@@ -8,9 +14,15 @@ var bell = new Audio("bell.mp3");
 function template(){
   document.getElementById("minutes").innerHTML = minutes;
   document.getElementById("seconds").innerHTML = seconds;
+
+  document.getElementById("session-text").innerHTML = sessionMinutes + ":00";
+  document.getElementById("break-text").innerHTML = breakMinutes + ":00";
 }
 
 function start(){
+
+  //Set flag to true so that the user must stop timer to edit time
+  flag = true;
   click.play();
 
   minutes = 24;
@@ -53,7 +65,65 @@ function start(){
       }
       seconds = 60;
     }
+  }
 
+}
+
+//Change the session length
+  function increaseSession(){
+    //Check if timer has already started
+    if(flag){
+      alert("Please stop and reset the timer to make any changes.");
+    }
+    if(sessionMinutes < 60){
+      sessionMinutes+=1;
+      console.log(sessionMinutes)
+      document.getElementById("session-text").innerHTML = sessionMinutes + ":00";
+      document.getElementById("minutes").innerHTML = sessionMinutes;
+    }else{
+      alert("Session cannot be longer than 60 minutes.")
+    }
+  }
+
+  function decreaseSession(){
+    //Check if timer has already started
+    if(flag){
+      alert("Please stop and reset the timer to make any changes.");
+    }
+    if(sessionMinutes > 5){
+      sessionMinutes-=1;
+      console.log(sessionMinutes)
+      document.getElementById("session-text").innerHTML = sessionMinutes + ":00";
+      document.getElementById("minutes").innerHTML = sessionMinutes;
+    }else{
+      alert("Session must be at least 5 minutes.")
+    }
 
   }
-}
+
+//Change the break length
+  function increaseBreak(){
+    //Check if timer has already started
+    if(flag){
+      alert("Please stop and reset the timer to make any changes.")
+    }
+    if(breakMinutes < 25){
+      breakMinutes+=1;
+      document.getElementById("break-text").innerHTML = breakMinutes + ":00";
+    }else{
+      alert("Break cannot be longer than 25 minutes.")
+    }
+  }
+
+  function decreaseBreak(){
+    //Check if timer has already started
+    if(flag){
+      alert("Please stop and reset the timer to make any changes.")
+    }
+    if(breakMinutes > 2){
+      breakMinutes-=1;
+      document.getElementById("break-text").innerHTML = breakMinutes + ":00"
+    }else{
+      alert("Break must be at least 2 minutes.")
+    }
+  }
